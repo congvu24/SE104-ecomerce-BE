ea72f8acbff7e88b2389dd8d3625ab9d6244064d
Vu Duong <19521110@gm.uit.edu.vn>
2021-06-21 16:07:51 +0000

    Update discount.js

--- routes/discount.js
+++ routes/discount.js
@@ -11,7 +11,7 @@ const adminAuth = require("../middleware/adminAuthMiddleware");
 
 var router = express.Router();
 
-router.get("/:code", adminAuth, getDiscountDetail);
+router.get("/:code", getDiscountDetail);
 router.get("/", adminAuth, getAllDiscount);
 router.post("/", adminAuth, createDiscount);
 router.patch("/:id", adminAuth, editDiscount);
